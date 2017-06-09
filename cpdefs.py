b336edc2fe778b68710449b412fcece11f1a6b95
Brandon Wynne <bmwynne@Brandons-iMac.local>
2017-06-09 11:07:55 -0500

    in working state. refactoring for non gpio and only serial tx rx

---
+++ cpdefs.py
@@ -0,0 +1,16 @@
+class CpDefs:
+    #Apn = "c1.korem2m.com"
+    #ApnUserid = ""
+    #ApnPassword = ""
+    #Server = "23.253.112.132"
+    #Server = "voidworx.com"
+    #Port = "1337"
+    #ServerFolder = "/pings"
+    #ModemPort = "/dev/ttyUSB0" # New Serial Aliasing (ls -l /dev)
+    ModemPort = "/dev/cu.UC-232AC"
+    ModemBaudrate = 9600
+    #RfPort = "/dev/ttyO2"
+    #RfBaudrate = 38400
+    
+    
+    
---
+++ main.py
@@ -0,0 +1,41 @@
+import Queue
+import time
+from cpconsole import CpConsole
+from cpmodem import CpModem
+from cpcomm import CpComm
+
+from datetime import datetime
+
+
+def modemDataReceived(data):
+    print data
+    
+def rfDataReceived(data):
+    print data
+    
+def inetDataReceived(data):
+    print data
+
+if __name__ == '__main__':
+
+    print "Modem init complete!"
+
+    modemThread = CpModem(modemDataReceived)
+    modemThread.start()
+    
+    commThread = CpComm(modemThread)
+    commThread.start()
+    
+    consoleThread = CpConsole(modemThread, commThread)
+    consoleThread.start()
+    
+    while(consoleThread.isAlive()):
+        time.sleep(.005)
+
+    print 'Exiting App...'
+    print 'Exiting App...'
+    exit()
+    
+    
+
+
